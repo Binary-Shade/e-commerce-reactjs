d9b38ee1bf25b4f9ac1638fb1d73e132a2d1fa88
suresh k <115919438+Binary-Shade@users.noreply.github.com>
2024-06-13 22:37:06 +0530

    search input added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaShoppingCart } from "react-icons/fa";
 import { cartContext } from '../App';
 
 export const Header = () => {
     const {cart} = useContext(cartContext)
+    const [search, setSearch] = useState('')
   return (
     <div className="header">
     <div className='logo'>
@@ -12,6 +13,17 @@ export const Header = () => {
     </div>
     <div className="navbar">
         <ul>
+            <li className='input-section'>
+            <form onSubmit={(e)=>e.preventDefault()}>
+                <label htmlFor="search">search products</label>
+                <input type="text"
+                className='search-box' 
+                value={search}
+                onChange={(e)=>setSearch(e.target.value)}
+                placeholder= 'search products'
+                />
+            </form>
+            </li>
             <li>
                 <Link to={'/'}>Home</Link>
             </li>
