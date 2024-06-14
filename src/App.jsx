359a3023848a30e2977de2bcead186d0c5eb023c
suresh k <115919438+Binary-Shade@users.noreply.github.com>
2024-06-14 21:40:57 +0530

    pagination view

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,56 @@
-import { Header } from "./components/Header"
-import { Route, Routes, useActionData } from 'react-router-dom' 
-import Home from "./components/Home"
-import Cart from "./components/Cart"
-import About from "./components/About"
-import Error from "./components/Error"
-import { createContext, useEffect, useState } from "react"
+import { Header } from "./components/Header";
+import { Route, Routes } from 'react-router-dom'; 
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createContext, useEffect, useState } from "react";
 
-//context creation
+// Context creation
 export const cartContext = createContext();
 
 function App() {
   const [items, setItems] = useState([]);
-  // fake api fr testing
-  const URL = 'https://fakestoreapi.com/products'
-  const [cart, setCart] = useState ([]) 
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [cart, setCart] = useState([]); 
+  const [search, setSearch] = useState('');
 
-  useEffect(()=>{
+  const URL = 'https://api.escuelajs.co/api/v1/products';
+
+  useEffect(() => {
     const fetchItems = async () => {
       const response = await fetch(URL);
       const data = await response.json();
-      setItems(data)
-      console.log(items);
-    } 
+      setItems(data);
+      console.log(data);
+      setFilteredItems(data);  // Set initial filtered items to all items
+    }; 
 
     fetchItems();
-  }, [])
+  }, []);
+
+  // Effect for filtering items based on search input
+  useEffect(() => {
+    const searchVal = search.toLowerCase();
+    const filteredProducts = items.filter((item) =>
+      item.title.toLowerCase().includes(searchVal)
+    );
+    setFilteredItems(filteredProducts);
+  }, [search, items]);
+
   return (
-    <cartContext.Provider value={{cart, setCart, items}}>
+    <cartContext.Provider value={{ cart, items: filteredItems, search, setSearch, setCart }}>
       <Header />
-      <div className="container">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart products = {cart} cart={cart} setCart={setCart}/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      </div>
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div> 
     </cartContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
